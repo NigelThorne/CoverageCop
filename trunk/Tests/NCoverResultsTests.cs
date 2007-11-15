cdde74aa7d2ac36553c7df2bff4dfadeab4cc19d
nigel_thorne <nigel_thorne@3f4cfd94-e13e-0410-983d-b8f1fa0f6271>
2007-11-15 09:38:34 +0000

    Added some tests for joining adjacent lines together in the coverage Report

--- trunk/Tests/NCoverResultsTests.cs
+++ trunk/Tests/NCoverResultsTests.cs
@@ -11,6 +11,7 @@ namespace NCoverCop.Tests
         private NCoverNode node4to4;
         private NCoverNode node5to5;
         private NCoverResults resultsAll;
+        private NCoverResults resultsNone;
         private NCoverResults results1to1n4to4;
 
         [SetUp]
@@ -21,16 +22,23 @@ namespace NCoverCop.Tests
             node4to4 = new NCoverNode(4, 0, 4, 1, "doc1", false, false);
             node5to5 = new NCoverNode(5, 0, 5, 1, "doc1", false, false);
 
+            resultsNone = new NCoverResults(new NCoverNode[] {});
             resultsAll = new NCoverResults(new NCoverNode[] { node1to1, node2to2, node4to4, node5to5 });
             results1to1n4to4 = new NCoverResults(new NCoverNode[] { node1to1, node4to4 });
 
         }
 
         [Test]
-        public void method_result_condition()
+        public void ReportNewUntestedCode_DisplaysUntestedLines()
         {
             Assert.AreEqual("Line 2-2 in doc1\nLine 5-5 in doc1\n", resultsAll.ReportNewUntestedCode(results1to1n4to4));
             Assert.AreEqual("", results1to1n4to4.ReportNewUntestedCode(resultsAll));
+        }
+
+        [Test]
+        public void ReportNewUntestedCode_JoinsBlocksOfuntestedCodeIntoASingleEntry()
+        {
+            Assert.AreEqual("Line 1-2 in doc1\nLine 4-5 in doc1\n", resultsAll.ReportNewUntestedCode(resultsNone));
         }
 
     }
