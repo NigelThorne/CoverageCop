f9b645836002420bd4caa9c0eddf560b0b2312d4
Nigel Thorne <nigelthorne@users.noreply.github.com>
2008-05-22 00:46:11 +0000

    update comparison to take into account methods being moved up and down in the file

---
+++ XmlNodeHelper.cs
@@ -0,0 +1,23 @@
+using System.Xml;
+
+
+namespace NCoverCop
+{
+    public class XmlNodeHelper
+    {
+        public static int GetIntAttribute(string name, XmlNode node)
+        {
+            return node.Attributes[name] != null ? int.Parse(node.Attributes[name].Value) : 0;
+        }
+
+        public static string GetStringAttribute(string name, XmlNode node)
+        {
+            return node.Attributes[name] != null ? node.Attributes[name].Value : "";
+        }
+
+        public static bool GetBoolAttribute(string name, XmlNode node)
+        {
+            return node.Attributes[name] != null && node.Attributes[name].Value != "false";
+        }
+    }
+}
