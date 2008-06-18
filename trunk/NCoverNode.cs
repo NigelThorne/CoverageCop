67f84250357dd73b0f394ea783f366ff1fb49038
nigel_thorne <nigel_thorne@3f4cfd94-e13e-0410-983d-b8f1fa0f6271>
2008-06-18 00:13:24 +0000

    Prefactoring towards handling new file format. extracting parser as separate class

--- trunk/NCoverNode.cs
+++ trunk/NCoverNode.cs
@@ -2,53 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
-using System.Xml;
 
 namespace NCoverCop
 {
-    public class NCoverFileParser
-    {
-        public NCoverNode ParseNode(XmlNode node, Regex documentPathIgnoreMatcher)
-        {
-            return new NCoverNode(
-                XmlNodeHelper.GetIntAttribute("line", node),
-                XmlNodeHelper.GetIntAttribute("column", node),
-                XmlNodeHelper.GetIntAttribute("endline", node),
-                XmlNodeHelper.GetIntAttribute("endcolumn", node),
-                XmlNodeHelper.GetStringAttribute("document", node),
-                XmlNodeHelper.GetIntAttribute("visitcount", node),
-                XmlNodeHelper.GetBoolAttribute("excluded", node),
-                XmlNodeHelper.GetStringAttribute("name", node.ParentNode),
-                XmlNodeHelper.GetIntAttribute("line", node.ParentNode.FirstChild),
-                XmlNodeHelper.GetStringAttribute("class", node.ParentNode),
-                documentPathIgnoreMatcher);
-        }
-
-        public NCoverResults Open(string coverageFile, Regex partOfPathToKeep)
-        {
-            List<INCoverNode> nodes = new List<INCoverNode>();
-            XmlDocument results = new XmlDocument();
-            if (File.Exists(coverageFile))
-            {
-                try
-                {
-                    results.LoadXml(File.ReadAllText(coverageFile));
-                }
-                catch (Exception ex)
-                {
-                    Console.Out.WriteLine(ex.Message);
-                }
-
-                foreach (XmlNode node in results.SelectNodes("//seqpnt"))
-                {
-                    nodes.Add(this.ParseNode(node, partOfPathToKeep));
-                }
-            }
-            return new NCoverResults(nodes);
-        }
-
-    }
-
     public class NCoverNode : INCoverNode
     {
         private readonly int column;
